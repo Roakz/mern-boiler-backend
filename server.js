10b2b4d59e44a33363b0d7a18c92883e7c5b1959
Roarkz <rory_bell_88@hotmail.com>
2020-01-09 13:54:29 +1000

    Axios is succesfullly integrated. Proxy removed and cors middleware inserted.

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
+require("dotenv").config();
+
 // - requiring express allows us to use the http methods and create a server to receive incoming requests
 //and send outgoing respponses. 
 // - importing the routes file which exports the express Router
 //  - mongoose is used for creating a schema and model for data validation. It also allows for the connection
 // to to MongoDB the document based database that is commonly used in the MERN stack M = Mongo
+
 const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 // creating an instance of express. We can do this now that we have required it
 const app = express()
 
 // assigning the express port to a constant. Anything except 3000 should workk fine. 3000 is the default 
 // port that us used for React which is used on the front end.
-const port = 5000
+const port = 3001
 
 // connecting to the MongoDB Database using mongoose. The catch block will log an error upon initial connection
 mongoose.connect('mongodb://localhost/mern-boilerplate', {
@@ -23,7 +27,12 @@ mongoose.connect('mongodb://localhost/mern-boilerplate', {
 // This will listen and log any errors after initial connection
 mongoose.connection.on('error', err => console.log(err))
 
-// Directing ecpress ot use the router from the routes.js file for app local serving
+// Cors middleware to enable cross orgin request from the front end
+app.use(cors({
+    origin: process.env.FRONT_END_DOMAIN
+}));
+
+// Directing express to use the router from the routes.js file for backend api serving.
 app.use(routes)
 
 // Turning the server on simply put. We tell express what port top listen on.In this case its 5000 which we set as the port
